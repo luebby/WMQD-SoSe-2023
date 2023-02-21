3469d5ee2c0d58b3d7d761daab9d2e2169019a13
luebby <karsten@statistix.org>
2023-02-21 13:53:59 +0100

    Init.

---
+++ setup/Pakete-installieren.R
@@ -0,0 +1,9 @@
+# Installiert die nötigen R Pakete
+install.packages(c("mosaic", "lsr", "GGally", "corrplot",
+                   "DT", "googlesheets4", "readxl", "here",
+                   "tinytex", "rmarkdown", "knitr", "webshot2"))
+
+# Installiert tinytex. Nötig für direktes `knit to pdf`
+# Siehe https://yihui.name/tinytex/
+tinytex::install_tinytex()
+
